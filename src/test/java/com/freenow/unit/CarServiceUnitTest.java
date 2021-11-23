6d5349ee359aea3c56db4b8ec551fd7dce0c6672
Mohammad Abu Musa Rabiul <silentrobi840@gmail.com>
2021-11-23 14:20:01 +0300

    added tests

--- src/test/java/com/freenow/unit/CarServiceUnitTest.java
+++ src/test/java/com/freenow/unit/CarServiceUnitTest.java
@@ -3,26 +3,33 @@ package com.freenow.unit;
 import com.freenow.controller.util.QueryParams;
 import com.freenow.dataaccessobject.CarRepository;
 import com.freenow.dataaccessobject.DriverRepository;
+import com.freenow.datatransferobject.UpdateCarDTO;
 import com.freenow.domainobject.CarDO;
 import com.freenow.domainobject.DriverDO;
 import com.freenow.domainvalue.EngineType;
 import com.freenow.domainvalue.Manufacturer;
+import com.freenow.exception.ConstraintsViolationException;
+import com.freenow.exception.EntityNotFoundException;
 import com.freenow.service.car.CarService;
 import com.freenow.service.car.DefaultCarService;
 import com.freenow.service.driver.DefaultDriverService;
 import com.freenow.service.driver.DriverService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 public class CarServiceUnitTest {
@@ -74,5 +81,123 @@ public class CarServiceUnitTest {
 
         // Then
         Assert.assertEquals(2, found.size());
+    }
+
+    @Test
+    public void GetCarByCarId() throws Exception {
+        CarDO carDO = new CarDO(
+                "12AXCD12",
+                5,
+                false,
+                "2016",
+                0.0,
+                EngineType.GAS,
+                Manufacturer.FORD
+        );
+        Mockito.when(carRepository.findById(carDO.getId())).thenReturn(Optional.of(carDO));
+        CarDO found = carService.find(carDO.getId());
+
+        Assert.assertEquals(carDO.getLicensePlate(), found.getLicensePlate());
+    }
+
+    @Test
+    public void GetExceptionByInvalidCarId() {
+        Long carId = 100L;
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            carService.find(carId);
+        });
+    }
+
+    @Test
+    public void createCarWithSameLicensePlate_ThrowException() {
+        CarDO carDO = new CarDO(
+                "12AXCD12",
+                5,
+                false,
+                "2016",
+                0.0,
+                EngineType.GAS,
+                Manufacturer.FORD
+        );
+        Mockito.when(carRepository.save(carDO)).thenThrow(DataIntegrityViolationException.class);
+
+        Assertions.assertThrows(ConstraintsViolationException.class, () -> {
+            carService.create(carDO);
+        });
+    }
+
+    @Test
+    public void createCar() throws Exception {
+        CarDO carDO = new CarDO(
+                "12AXCD12",
+                5,
+                false,
+                "2016",
+                0.0,
+                EngineType.GAS,
+                Manufacturer.FORD
+        );
+        Mockito.when(carRepository.save(carDO)).thenReturn(carDO);
+
+        CarDO newCarDO = carService.create(carDO);
+
+        Assert.assertEquals(carDO.getLicensePlate(), newCarDO.getLicensePlate());
+    }
+
+    @Test
+    public void updateCar_shouldThrowExceptionForSameLicensePlate(){
+        CarDO carDO = new CarDO(
+                "12AXCD12",
+                5,
+                false,
+                "2016",
+                7.9,
+                EngineType.DIESEL,
+                Manufacturer.FORD
+        );
+
+        Mockito.when(carRepository.findById(carDO.getId())).thenReturn(Optional.of(carDO));
+        Mockito.when(carRepository.save(carDO)).thenThrow(DataIntegrityViolationException.class);;
+
+        UpdateCarDTO updateCarDTO = new UpdateCarDTO();
+        updateCarDTO.setEngineType(EngineType.DIESEL);
+        updateCarDTO.setLicensePlate("12AXCD12");
+        updateCarDTO.setRating(7.9);
+
+        Assertions.assertThrows(ConstraintsViolationException.class, () -> {
+            carService.update(carDO.getId(), updateCarDTO);
+        });
+    }
+
+    @Test
+    public void updateCar_shouldThrowExceptionForInvalidCarId(){
+
+        UpdateCarDTO updateCarDTO = new UpdateCarDTO();
+        updateCarDTO.setEngineType(EngineType.DIESEL);
+        updateCarDTO.setLicensePlate("12AXCD12");
+        updateCarDTO.setRating(7.9);
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            carService.update(100L, updateCarDTO);
+        });
+    }
+
+    @Test
+    public void deleteCarShouldThrowError() {
+        CarDO carDO = new CarDO(
+                "12AXCD12",
+                5,
+                false,
+                "2016",
+                7.9,
+                EngineType.DIESEL,
+                Manufacturer.FORD
+        );
+        Mockito.when(carRepository.findById(carDO.getId())).thenReturn(Optional.of(carDO));
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            carService.delete(12L);
+        });
     }
 }
--- src/test/java/com/freenow/unit/DriverServiceUnitTest.java
+++ src/test/java/com/freenow/unit/DriverServiceUnitTest.java
@@ -137,5 +137,4 @@ public class DriverServiceUnitTest {
             driverService.delete(12L);
         });
     }
-
 }
