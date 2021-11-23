93178b4d959119a01c5e9578aee2bccbb2862532
Mohammad Abu Musa Rabiul <mohammad.rabiul@tokeninc.com>
2021-11-24 00:13:56 +0300

    added docs

--- src/main/java/com/freenow/security/service/CustomUserDetailService.java
+++ src/main/java/com/freenow/security/service/CustomUserDetailService.java
@@ -1,5 +1,8 @@
 package com.freenow.security.service;
 
+import com.freenow.exception.CarAlreadyInUseException;
+import com.freenow.exception.DriverOfflineException;
+import com.freenow.exception.EntityNotFoundException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +14,14 @@ import java.util.ArrayList;
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 
+    /**
+     * load user by username.
+     *
+     * @param username
+     * @throws UsernameNotFoundException  if no driver with the given id was found.
+     */
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return new User("admin", "12345", new ArrayList<>());
     }
 }
